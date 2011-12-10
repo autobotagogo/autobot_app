7904dd31d88ba06a78a0f59bba2b22ae931ac11e
autobotagogo <mai@Christina-Hoangs-iMac.local>
2011-12-10 14:43:41 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AutobotApp::Application.config.secret_token = '8dfdbaed6496913b44e542006f705e29430c52585bc74aa6de85392f81633394a644647ce35fe949b2dd35df7b23212cdd4fb9aa8f484efc457e6e2ec3b45e57'
