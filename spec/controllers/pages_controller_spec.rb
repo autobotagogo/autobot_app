38043dbe024d364cf7b4878138b6bdc64e5fcd4f
autobotagogo <mai@Christina-Hoangs-iMac.local>
2011-12-11 01:20:12 -0600

    completed chapter 4

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -12,7 +12,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Home")
+                        :content => @base_title + " | Home")
     end
   end
 
@@ -25,8 +25,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-                        :content =>
-                          "Ruby on Rails Tutorial Sample App | Contact")
+                        :content => @base_title + " | Contact")
     end
   end
 
@@ -39,8 +38,7 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-                        :content =>
-                          "Ruby on Rails Tutorial Sample App | About")
+                        :content => @base_title + " | About")
     end
   end
   
@@ -53,8 +51,7 @@ describe PagesController do
     it "should have the right title" do
       get 'help'
       response.should have_selector("title",
-                        :content =>
-                          "Ruby on Rails Tutorial Sample App | Help")
+                        :content => @base_title + " | Help")
     end
   end
 end
